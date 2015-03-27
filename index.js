059d8a872c84fea4514efa8317424f3c7c0908c0
Ryan Clark <ryan.clark.j@gmail.com>
2015-03-27 16:00:23 -0700

    first

---
+++ index.js
@@ -0,0 +1,192 @@
+var test = require('tape');
+var crypto = require('crypto');
+var AWS = require('aws-sdk');
+var _ = require('underscore');
+var kinesalite = require('kinesalite')({
+  createStreamMs: 0,
+  ssl: false
+});
+var queue = require('queue-async');
+
+module.exports = function(projectName, shards, region) {
+  var live = !!region;
+
+  var options = live ? { region: region } : {
+    region: 'fake',
+    accessKeyId: 'fake',
+    secretAccessKey: 'fake',
+    endpoint: 'http://localhost:7654'
+  };
+
+  var kinesis = {};
+
+  kinesis.kinesis = new AWS.Kinesis(options);
+
+  kinesis.streamName = [
+    'test',
+    projectName,
+    crypto.randomBytes(4).toString('hex')
+  ].join('-');
+
+  var Readable = require('kinesis-readable')(_({
+    streamName: kinesis.streamName,
+    streamRegion: options.region
+  }).extend(options));
+
+  var streamRunning = false;
+
+  function start(assert, callback) {
+    if (live) assert.timeoutAfter(300000);
+    if (streamRunning) return callback();
+
+    function ready(err) {
+      if (err) throw err;
+
+      check(kinesis.kinesis, kinesis.streamName, function(err, status) {
+        if (err) throw err;
+        if (status !== 'ACTIVE') return setTimeout(ready, 1000);
+
+        streamRunning = true;
+
+        shardids(kinesis.kinesis, kinesis.streamName, function(err, shardids) {
+          if (err) throw err;
+
+          kinesis.shards = shardids.map(function(id) {
+            return new Readable(id);
+          });
+
+          assert.end();
+        });
+      });
+    }
+
+    if (live) return create(kinesis.kinesis, kinesis.streamName, shards, ready);
+
+    kinesalite.listen(7654, function(err) {
+      if (err) throw err;
+      create(kinesis.kinesis, kinesis.streamName, shards, ready);
+    });
+  }
+
+  kinesis.start = function() {
+    test('[kinesis-test] create stream', function(assert) {
+      start(assert, function() {
+        assert.end();
+      });
+    });
+  };
+
+  kinesis.delete = function() {
+    test('[kinesis-test] delete stream', function(assert) {
+      if (live) assert.timeoutAfter(300000);
+      if (!streamRunning) return assert.end();
+
+      function dead(err) {
+        if (err) console.log('delete errored');
+        if (err) throw err;
+
+        check(kinesis.kinesis, kinesis.streamName, function(err, status) {
+          if (err) throw err;
+          if (status !== 'DOESNOTEXIST') return setTimeout(dead, 1000);
+          streamRunning = false;
+          assert.end();
+        });
+      }
+
+      var q = queue();
+      kinesis.shards.forEach(function(shard) {
+        q.defer(function(next) {
+          shard.close(next);
+        });
+      });
+
+      q.awaitAll(function(err) {
+        if (err) throw err;
+        delete kinesis.shards;
+        destroy(kinesis.kinesis, kinesis.streamName, dead);
+      });
+    });
+  };
+
+  kinesis.load = function(fixtures) {
+    test('[kinesis-test] load fixtures', function(assert) {
+      if (!streamRunning) start(assert, load);
+      else load();
+
+      function load() {
+        kinesis.kinesis.putRecords({
+          StreamName: kinesis.streamName,
+          Records: fixtures
+        }, function(err) {
+          if (err) throw err;
+          assert.end();
+        });
+      }
+    });
+  };
+
+  kinesis.test = function(name, fixtures, callback) {
+    kinesis.delete();
+    kinesis.start();
+
+    if (typeof fixtures === 'function') {
+      callback = fixtures;
+      fixtures = null;
+    }
+
+    if (fixtures && fixtures.length) kinesis.load(fixtures);
+
+    test(name, callback);
+    kinesis.delete();
+  };
+
+  if (!live) kinesis.close = function() {
+    test('[kinesis-test] close kinesalite', function(assert) {
+      kinesalite.close(function(err) {
+        if (err) throw err;
+        assert.end();
+      });
+    });
+  };
+
+  return kinesis;
+};
+
+function create(kinesis, name, shards, callback) {
+  kinesis.createStream({
+    StreamName: name,
+    ShardCount: shards
+  }, callback);
+}
+
+function destroy(kinesis, name, callback) {
+  kinesis.deleteStream({
+    StreamName: name
+  }, callback);
+}
+
+function check(kinesis, name, callback) {
+  kinesis.describeStream({
+    StreamName: name
+  }, function(err, data) {
+    if (err && err.code === 'ResourceNotFoundException')
+      return callback(null, 'DOESNOTEXIST');
+    if (err) {
+      console.log('describe errored');
+      return callback(err);
+    }
+
+    callback(null, data.StreamDescription.StreamStatus);
+  });
+}
+
+function shardids(kinesis, name, callback) {
+  kinesis.describeStream({
+    StreamName: name
+  }, function(err, data) {
+    if (err) return callback(err);
+    callback(null, data.StreamDescription.Shards.map(function(shard) {
+      return shard.ShardId;
+    }));
+  });
+}
---
+++ test/index.test.js
@@ -0,0 +1,181 @@
+var test = require('tape');
+var crypto = require('crypto');
+
+var project = crypto.randomBytes(4).toString('hex');
+
+var mocked = require('..')(project, 1);
+var live = require('..')(project, 1, 'us-east-1');
+
+test('sets streamName', function(assert) {
+  var re = new RegExp('test-' + project + '-[a-zA-Z0-9]{8}');
+  assert.ok(re.test(mocked.streamName), 'mocked sets streamName');
+  assert.ok(re.test(live.streamName), 'live sets streamName');
+  assert.end();
+});
+
+test('provides sdk', function(assert) {
+  assert.ok(mocked.kinesis, 'provides kinesis');
+  assert.end();
+});
+
+mocked.start();
+
+test('mocked start', function(assert) {
+  mocked.kinesis.listStreams({}, function(err, data) {
+    if (err) throw err;
+    assert.deepEqual(data.StreamNames, [mocked.streamName], 'creates the stream');
+    assert.equal(mocked.shards.length, 1, 'creates a readable stream for the shard');
+    assert.end();
+  });
+});
+
+mocked.load([{ Data: 'hello', PartitionKey: 'a' }]);
+
+test('mocked fixture load', function(assert) {
+  assert.plan(3);
+
+  mocked.shards[0].on('data', function(item) {
+    assert.deepEqual(item.Data, new Buffer('hello'), 'expected data');
+    assert.equal(item.PartitionKey, 'a', 'expected partition key');
+    assert.ok(item.SequenceNumber, 'has sequence number');
+  });
+});
+
+mocked.delete();
+
+test('mocked delete', function(assert) {
+  mocked.kinesis.listStreams({}, function(err, data) {
+    if (err) throw err;
+    assert.equal(data.StreamNames.length, 0, 'deleted the stream');
+    assert.notOk(mocked.shards, 'close the streams');
+    assert.end();
+  });
+});
+
+mocked.test('mocked test', function(assert) {
+  assert.pass('runs the test');
+  mocked.kinesis.listStreams({}, function(err, data) {
+    if (err) throw err;
+    assert.deepEqual(data.StreamNames, [mocked.streamName], 'creates the stream');
+    assert.equal(mocked.shards.length, 1, 'creates a readable stream for the shard');
+    assert.end();
+  });
+});
+
+test('mocked test cleanup', function(assert) {
+  mocked.kinesis.listStreams({}, function(err, data) {
+    if (err) throw err;
+    assert.equal(data.StreamNames.length, 0, 'deleted the stream');
+    assert.notOk(mocked.shards, 'close the streams');
+    assert.end();
+  });
+});
+
+mocked.test('mocked test with fixtures', [{ Data: 'hello', PartitionKey: 'a' }], function(assert) {
+  assert.pass('runs the test');
+  mocked.kinesis.listStreams({}, function(err, data) {
+    if (err) throw err;
+    assert.deepEqual(data.StreamNames, [mocked.streamName], 'creates the stream');
+    assert.equal(mocked.shards.length, 1, 'creates a readable stream for the shard');
+    mocked.shards[0].on('data', function(item) {
+      assert.deepEqual(item.Data, new Buffer('hello'), 'expected data');
+      assert.equal(item.PartitionKey, 'a', 'expected partition key');
+      assert.ok(item.SequenceNumber, 'has sequence number');
+      assert.end();
+    });
+  });
+});
+
+test('mocked test with fixtures cleanup', function(assert) {
+  mocked.kinesis.listStreams({}, function(err, data) {
+    if (err) throw err;
+    assert.equal(data.StreamNames.length, 0, 'deleted the stream');
+    assert.notOk(mocked.shards, 'close the streams');
+    assert.end();
+  });
+});
+
+mocked.close();
+
+test('mocked close kinesalite', function(assert) {
+  assert.end();
+});
+
+live.start();
+
+test('live start', function(assert) {
+  live.kinesis.listStreams({}, function(err, data) {
+    if (err) throw err;
+    assert.ok(data.StreamNames.indexOf(live.streamName), 'creates the stream');
+    assert.equal(live.shards.length, 1, 'creates a readable stream for the shard');
+    assert.end();
+  });
+});
+
+live.load([{ Data: 'hello', PartitionKey: 'a' }]);
+
+test('live fixture load', function(assert) {
+  assert.plan(3);
+
+  live.shards[0].on('data', function(item) {
+    assert.deepEqual(item.Data, new Buffer('hello'), 'expected data');
+    assert.equal(item.PartitionKey, 'a', 'expected partition key');
+    assert.ok(item.SequenceNumber, 'has sequence number');
+  });
+});
+
+live.delete();
+
+test('live delete', function(assert) {
+  live.kinesis.listStreams({}, function(err, data) {
+    if (err) throw err;
+    assert.equal(data.StreamNames.indexOf(live.streamName), -1, 'deleted the stream');
+    assert.notOk(live.shards, 'close the streams');
+    assert.end();
+  });
+});
+
+live.test('live test', function(assert) {
+  assert.pass('runs the test');
+  live.kinesis.listStreams({}, function(err, data) {
+    if (err) throw err;
+    assert.ok(data.StreamNames.indexOf(live.streamName), 'creates the stream');
+    assert.equal(live.shards.length, 1, 'creates a readable stream for the shard');
+    assert.end();
+  });
+});
+
+test('live test cleanup', function(assert) {
+  live.kinesis.listStreams({}, function(err, data) {
+    if (err) throw err;
+    assert.equal(data.StreamNames.indexOf(live.streamName), -1, 'deleted the stream');
+    assert.notOk(live.shards, 'close the streams');
+    assert.end();
+  });
+});
+
+live.test('live test with fixtures', [{ Data: 'hello', PartitionKey: 'a' }], function(assert) {
+  assert.pass('runs the test');
+  live.kinesis.listStreams({}, function(err, data) {
+    if (err) throw err;
+    assert.ok(data.StreamNames.indexOf(live.streamName), 'creates the stream');
+    assert.equal(live.shards.length, 1, 'creates a readable stream for the shard');
+    live.shards[0].on('data', function(item) {
+      assert.deepEqual(item.Data, new Buffer('hello'), 'expected data');
+      assert.equal(item.PartitionKey, 'a', 'expected partition key');
+      assert.ok(item.SequenceNumber, 'has sequence number');
+      assert.end();
+    });
+  });
+});
+
+test('live test with fixtures cleanup', function(assert) {
+  live.kinesis.listStreams({}, function(err, data) {
+    if (err) throw err;
+    assert.equal(data.StreamNames.indexOf(live.streamName), -1, 'deleted the stream');
+    assert.notOk(live.shards, 'close the streams');
+    assert.end();
+  });
+});
+
+live.delete();
